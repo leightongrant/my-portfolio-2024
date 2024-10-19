dcea41f5f497fdc7936ba4695f341a8dc2fb6826
Leighton Grant <oneal.grant@outlook.com>
2024-10-19 11:56:32 +0100

    Installs react-router, react-router-dom and setup components and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout'
+import Homepage from './pages/Homepage'
+import About from './pages/About'
+import Contact from './pages/ Contact'
+import NotFound from './pages/NotFound'
+
+const router = createBrowserRouter([
+	{
+		element: <Layout />,
+		children: [
+			{ path: '/', element: <Homepage /> },
+			{ path: '/about', element: <About /> },
+			{ path: '/contact', element: <Contact /> },
+			{ path: '*', element: <NotFound /> },
+		],
+	},
+])
 function App() {
-	return (
-		<>
-			<h1>Welcome to my portfolio</h1>
-		</>
-	)
+	return <RouterProvider router={router} />
 }
 
 export default App
